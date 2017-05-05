856c72317245589122a16c900a3cf8c0d9cefae3
calwen <hooljun@163.com>
2017-05-06 01:02:41 +0800

    change one file

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Hello{
 	
 	public static void main(String[] args){
-		System.out.println("hello git!!!");
+		System.out.println("hello 哈哈哈!!!");
 		System.out.println("hello git!!!");
 	}
 }
